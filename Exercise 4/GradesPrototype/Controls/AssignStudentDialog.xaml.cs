356fc3b8e1477a1b115d7d7a01b7b9baf67ae07e
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2018-02-15 14:52:56 +0100

    School Module 4 Opdracht 4

---
+++ Exercise 4/GradesPrototype/Controls/AssignStudentDialog.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Controls
+{
+    /// <summary>
+    /// Interaction logic for AssignStudentDialog.xaml
+    /// </summary>
+    public partial class AssignStudentDialog : Window
+    {
+        public AssignStudentDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Refresh()
+        {
+            var unassignedStudents = from s in DataSource.Students      //#2 from the DataSource Students list
+                                     where s.TeacherID == 0             //#3 where the TeacherID is 0 (meaning that these students don't have a teacher assigned to them).
+                                     select s;                          //#1 Select the students
+
+            if (unassignedStudents.Count() == 0)            //If there are no students without a teacher, the list wont show up.
+            {
+                txtMessage.Visibility = Visibility.Visible;
+                list.Visibility = Visibility.Collapsed;
+            }
+            else                                            //If there are students without a teacher however, the list will show up.
+            {
+                txtMessage.Visibility = Visibility.Collapsed;
+                list.Visibility = Visibility.Visible;
+
+                list.ItemsSource = unassignedStudents;
+            }
+        }
+
+        private void AssignStudentDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Student_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Button studentClicked = sender as Button;
+                int studentID = (int)studentClicked.Tag;
+
+                Student student = (from s in DataSource.Students        //#2 from the DataSource Students list
+                                   where s.StudentID == studentID       //#3 where the ID matches.
+                                   select s).First();                   //#1 Selects the student
+
+                string message = String.Format("Do you wish to add {0} {1} to your class?", student.FirstName, student.LastName);
+                MessageBoxResult reply = MessageBox.Show(message, "Confirm.", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (reply == MessageBoxResult.Yes)
+                {
+                    int teacherID = SessionContext.CurrentTeacher.TeacherID;            //Sets the teacherID to the Current Teacher's ID.
+                    
+                    SessionContext.CurrentTeacher.EnrollInClass(student);               //Student is added to the Current Teacher's class via the EnrollInClass method.
+
+                    Refresh();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Couldn't enroll the student.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the dialog box
+            this.Close();
+        }
+    }
+}
---
+++ Exercise 4/GradesPrototype/Views/StudentProfile.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Controls;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentProfile.xaml
+    /// </summary>
+    public partial class StudentProfile : UserControl
+    {
+        public StudentProfile()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        public event EventHandler Back;
+        #endregion
+
+        #region Events
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            // If the user is not a teacher, do nothing (the button should not appear anyway)
+            if (SessionContext.UserRole != Role.Teacher)
+            {
+                return;
+            }
+
+            // If the user is a teacher, raise the Back event
+            // The MainWindow page has a handler that catches this event and returns to the Students page
+            if (Back != null)
+            {
+                Back(sender, e);
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionContext.UserRole != Role.Teacher)                //Makes the button do nothing if a Student clicks on it, since it returns immediately when the role is not a Teacher.
+            {
+                return;
+            }
+
+            try
+            {
+                string message = String.Format("Remove {0} {1}", SessionContext.CurrentStudent.FirstName,       //Messagebox that creates extra validation for removing a student by asking the teacher if they really want to removed said student.
+                    SessionContext.CurrentStudent.LastName); MessageBoxResult reply = 
+                    MessageBox.Show(message, "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (reply == MessageBoxResult.Yes)
+                {
+                    SessionContext.CurrentTeacher.RemoveFromClass(SessionContext.CurrentStudent);
+
+                    if (Back != null)
+                    {
+                        Back(sender, e);
+                    }
+                }
+            }
+            catch (Exception ex)        //Whenever an unexpected error occurs when removing a student.
+            {
+                MessageBox.Show(ex.Message, "There was an error removing this student from the class.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AddGrade_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionContext.UserRole != Role.Teacher)        //Makes the button do nothing if a Student clicks on it, since it returns immediately when the role is not a Teacher.
+            {
+                return;
+            }
+
+            try
+            {
+                GradeDialog gd = new GradeDialog();
+
+                if (gd.ShowDialog().Value)
+                {
+                    Grade newGrade = new Grade();       //Creates a new Grade object and stores (and converts if necessary) the new grades in it.
+                    newGrade.AssessmentDate = gd.assessmentDate.SelectedDate.Value.ToString("d");
+                    newGrade.SubjectName = gd.subject.SelectedValue.ToString();
+                    newGrade.Assessment = gd.assessmentGrade.Text;
+                    newGrade.Comments = gd.comments.Text;
+                    
+                    DataSource.Grades.Add(newGrade);        //Adds the grade to the DataSource list.
+                    
+                    SessionContext.CurrentStudent.AddGrade(newGrade);       //Saves the newly added grade to the current student.
+                    
+                    Refresh();      //Execute the Refresh method so the added grade appears.
+                }
+            }
+            catch (Exception ex)        //Whenever an unexpected error occurs when adding a grade.
+            {
+                MessageBox.Show(ex.Message, "There was an error adding the grade to this student.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        #endregion
+
+        // Display the details for the current student (held in SessionContext.CurrentStudent), including the grades for the student
+        public void Refresh()
+        {
+            // Bind the studentName StackPanel to display the details of the student in the TextBlocks in this panel
+            studentName.DataContext = SessionContext.CurrentStudent;
+
+            // If the current user is a student, hide the Back, Remove, and Add Grade buttons
+            // (these features are only applicable to teachers)
+            if (SessionContext.UserRole == Role.Student)
+            {
+                btnBack.Visibility = Visibility.Hidden;
+                btnRemove.Visibility = Visibility.Hidden;
+                btnAddGrade.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                btnBack.Visibility = Visibility.Visible;
+                btnRemove.Visibility = Visibility.Visible;
+                btnAddGrade.Visibility = Visibility.Visible;
+            }
+
+            // Find all the grades for the student
+            List<Grade> grades = new List<Grade>();
+            foreach (Grade grade in DataSource.Grades)
+            {
+                if (grade.StudentID == SessionContext.CurrentStudent.StudentID)
+                {
+                    grades.Add(grade);
+                }
+            }
+            
+            // Display the grades in the studentGrades ItemsControl by using databinding
+            studentGrades.ItemsSource = grades;
+        }
+    }
+}
